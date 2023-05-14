0272010e575129783708abd87481de8361567102
congnghiahieu <21020540@vnu.edu.vn>
2023-05-14 23:43:37 +0700

    Test deploy 1

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ['http://localhost:5000', 'http://localhost:3000'];
+const allowedOrigins = [
+    'http://localhost:5000',
+    'http://localhost:3000',
+    'https://wygo.pagekite.me',
+];
 
 module.exports = allowedOrigins;

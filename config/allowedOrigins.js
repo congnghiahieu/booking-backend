e419f1980f6528dd01655861a17a655bf9dfa100
congnghiahieu <21020540@vnu.edu.vn>
2023-05-15 08:21:37 +0700

    Fix to deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5000',
     'http://localhost:3000',
-    'https://wygo.pagekite.me',
+    'https://wygo.onrender.com',
 ];
 
 module.exports = allowedOrigins;
